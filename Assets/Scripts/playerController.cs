3c911d7737873589cd01d792cda64c3bd42cb44e
JRenGomez <jacobrenGomez@gmail.com>
2025-03-06 11:24:24 -0600

    changes player script to better jump
    Made some logic errors from having to rely on animation to move and jump. this works better

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -1,67 +1,49 @@
+using System;
 using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
-    // movement variables 
-    public float maxSpeed;
+    private Rigidbody2D rb;
+    public float speed;
+    private float InputX;
 
-    // jumping variables
-    bool grounded = false;
-    float groundCheckRadius = 0.2f;
-    public LayerMask groundLayer;
-    public Transform groundCheck;
-    public float jumpHeight;
+    public float jumpForce;
+    [SerializeField]
+    bool isGrounded;
 
-    private Rigidbody2D myRB;
-    bool facingRight;
-     Animator myAnim;
-    void Start()
+    private void Start()
     {
-        myRB = GetComponent<Rigidbody2D>();
-        myAnim = GetComponent<Animator>();
-        facingRight = true;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        InputX = Input.GetAxisRaw("Horizontal");
+
+        rb.velocity = new Vector2(InputX * speed, rb.velocity.y); 
     }
 
     private void Update()
     {
-        if (grounded && Input.GetAxis("Jump")>0) {
-            grounded = false;
-            myAnim.SetBool("isGrounded", grounded);
-            myRB.AddForce(new Vector2(0, jumpHeight));
-
+        if (isGrounded == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = Vector2.up * jumpForce;
         }
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        //check if we are grounded - if no, then we are falling
-        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
-        myAnim.SetBool("isGrounded", grounded);
-        
-        myAnim.SetFloat("verticalSpeed", myRB.velocity.y);
+        isGrounded = true;
+        print("Grouneded");
 
-
-
-        float move = Input.GetAxis("Horizontal");
-        myRB.velocity = new Vector2(move*maxSpeed, myRB.velocity.y);
-        if (move > 0 && !facingRight)
-        {
-            flip();
-        }
-        else if (move < 0 && facingRight)
-        {
-            flip();
-
-        }
     }
 
-    void flip()
+    private void OnCollisionExit2D(Collision2D collision)
     {
-        facingRight =!facingRight;
-        Vector2 theScale = transform.localScale;
-        theScale.x *= -1;
-        transform.localScale = theScale;
+        isGrounded= false;
+        print("Not Grouneded");
+
     }
 }
+
