9f4728acd0da12ecef44dba0dfc48a6c850c4642
JRenGomez <jacobrenGomez@gmail.com>
2025-03-06 00:10:29 -0600

    creating player controller

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -3,22 +3,44 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // movement variables 
     public float maxSpeed;
+
+    // jumping variables
+    bool grounded = false;
+    float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+    public Transform groundCheck;
+    public float jumpHeight;
+
     private Rigidbody2D myRB;
     bool facingRight;
-    //Animator myAnimator;
+     Animator myAnim;
     void Start()
     {
         myRB = GetComponent<Rigidbody2D>();
-       // myAnimator = GetComponent<Animator>();
-
+        // myAnimator = GetComponent<Animator>();
         facingRight = true;
+    }
+
+    private void Update()
+    {
+        if (grounded && Input.GetAxis("Jump")>0) {
+            grounded = false;
+            myRB.AddForce(new Vector2(0, jumpHeight));
+
+        }
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        //check if we are grounded - if no, then we are falling
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+        myAnim.SetFloat("verticalSpeed", myRB.velocity.y);
+
+
+
         float move = Input.GetAxis("Horizontal");
         myRB.velocity = new Vector2(move*maxSpeed, myRB.velocity.y);
         if (move > 0 && !facingRight)
